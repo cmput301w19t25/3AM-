7f9f72ae114ae50dbd0070924946cb4c7fc80637
nickdchee <nickdchee@gmail.com>
2019-02-15 15:12:39 -0700

    empty project

---
+++ app/src/main/java/comnickdchee/github/a3am/BorrowedActivity.java
@@ -0,0 +1,13 @@
+package comnickdchee.github.a3am;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BorrowedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_borrowed);
+    }
+}
