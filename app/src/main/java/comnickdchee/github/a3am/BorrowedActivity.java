c51d05f1db47dc3f6339dbb56f8277b70bf77d31
nickdchee <nickdchee@gmail.com>
2019-02-18 16:00:06 -0700

    added toolbar

--- app/src/main/java/comnickdchee/github/a3am/BorrowedActivity.java
+++ app/src/main/java/comnickdchee/github/a3am/BorrowedActivity.java
@@ -2,12 +2,20 @@ package comnickdchee.github.a3am;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class BorrowedActivity extends AppCompatActivity {
+    // TODO: Change this so that we get the username from backend
+    // and set the action bar's title to it.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_borrowed);
+
+        // override the default action bar, allowing us to use
+        // action bar commands
+        setSupportActionBar((Toolbar) findViewById(R.id.navToolbar));
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 }
