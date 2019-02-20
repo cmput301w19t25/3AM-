666b562891e2aedc94d51f9c6279b46de9736721
zaheenrahman <zaheen@ualberta.ca>
2019-02-19 18:07:43 -0700

    Rigging buttons and authenticating firebase

--- app/src/main/java/comnickdchee/github/a3am/BorrowedActivity.java
+++ app/src/main/java/comnickdchee/github/a3am/BorrowedActivity.java
@@ -1,13 +1,28 @@
 package comnickdchee.github.a3am;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class BorrowedActivity extends AppCompatActivity {
+import com.google.android.gms.common.oob.SignUp;
+
+public class BorrowedActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_borrowed);
+
+        findViewById(R.id.RegisterBtn).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.RegisterBtn:
+                startActivity(new Intent(this, SignUpActivity.class));
+                break;
+        }
     }
 }
---
+++ app/src/main/java/comnickdchee/github/a3am/SignUpActivity.java
@@ -0,0 +1,13 @@
+package comnickdchee.github.a3am;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
