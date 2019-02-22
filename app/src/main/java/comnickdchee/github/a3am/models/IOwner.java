a0dd96d1c871a12b36f0267881c58a1d9db72029
nickdchee <nickdchee@gmail.com>
2019-02-22 16:08:59 -0700

    added Owner and Borrower interface and User class

---
+++ app/src/main/java/comnickdchee/github/a3am/models/IOwner.java
@@ -0,0 +1,27 @@
+package comnickdchee.github.a3am.models;
+
+import android.media.Image;
+
+public interface IOwner {
+    /** Owner can accept a request. */
+    void acceptRequest(Book book, User user);
+
+    /** Owner can reject a request. */
+    void rejectRequest(Book book, User user);
+    
+    /** Owner adds book to MyBooks. */
+    void addOwnedBook(Book book);
+
+    /** Owner can remove books from MyBooks. */
+    void removeOwnedBook(Book book);
+
+    /** Sets the location of the specific exchange. */
+    void setLocation(Exchange exchange, String location);
+
+    /** Sets the image of the specific user's owned book. */
+    void setImage(int bookID, Image image);
+
+    /** Removes the image attached to the user's owned book
+     *  based on the actual book. */
+    void removeImage(int bookID);
+}
---
+++ app/src/main/java/comnickdchee/github/a3am/models/User.java
@@ -0,0 +1,90 @@
+package comnickdchee.github.a3am.models;
+
+import android.media.Image;
+import android.media.Rating;
+
+import java.util.ArrayList;
+
+public class User implements IOwner, IBorrower {
+
+    private String username;            // unique username
+    private String phoneNumber;
+    private String email;
+    private String name;
+    private String address;
+    private ArrayList<Book> ownedBooks;     // list of books owned by user
+    private ArrayList<Book> requestedBooks; // list of books that user is requesting
+    private Rating rating;                  // wow feature: user rating
+
+    /** Ctor */
+    User(String username, String phoneNumber, String email, String name,
+         String address) {
+        this.username = username;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.name = name;
+        this.address = address;
+
+        ownedBooks = new ArrayList<Book>();
+        requestedBooks = new ArrayList<Book>();
+    }
+
+    /** No address specified */
+    User(String username, String phoneNumber, String email, String name) {
+        this.username = username;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.name = name;
+
+        ownedBooks = new ArrayList<Book>();
+        requestedBooks = new ArrayList<Book>();
+    }
+
+    @Override
+    public void acceptRequest(Book book, User user) {
+    }
+
+    @Override
+    public void rejectRequest(Book book, User user) {
+    }
+
+    @Override
+    public void addOwnedBook(Book book) {
+        ownedBooks.add(book);
+    }
+
+    @Override
+    public void removeOwnedBook(Book book) {
+        for (int i = 0; i < ownedBooks.size(); ++i) {
+            if (ownedBooks.get(i) == book) {
+                ownedBooks.remove(i);
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void addRequestedBook(Book book) {
+        requestedBooks.add(book);
+    }
+
+    @Override
+    public void returnBook(int bookID) {
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+
+    @Override
+    public void setLocation(Exchange exchange, String location) {
+    }
+
+    @Override
+    public void setImage(int bookID, Image image) {
+    }
+
+    @Override
+    public void removeImage(int bookID) {
+    }
+}
