b4c2af92379b37fba57abf4e32c4793ec0570966
ismaeelBM <ismaeel@ualberta.ca>
2019-02-22 17:28:34 -0700

    Created UserUnitTest class

---
+++ app/src/test/java/comnickdchee/github/a3am/UserUnitTest.java
@@ -0,0 +1,19 @@
+package comnickdchee.github.a3am;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class UserUnitTest {
+    @Test
+    public void test_acceptRequests()
+    {
+        book = new Book("12345",);
+        assertEquals(4, 2 + 2);
+    }
+}
