e474187f31d3e7fa7fac8517cafed80a34cc4aa4
nickdchee <nickdchee@gmail.com>
2019-02-27 23:05:07 -0700

    refactored names of mainactivity

--- app/src/main/java/comnickdchee/github/a3am/Activities/HomepageActivity.java
+++ app/src/main/java/comnickdchee/github/a3am/Activities/HomepageActivity.java
@@ -6,13 +6,13 @@ import android.support.v7.widget.Toolbar;
 
 import comnickdchee.github.a3am.R;
 
-public class BorrowedActivity extends AppCompatActivity {
+public class HomepageActivity extends AppCompatActivity {
     private Toolbar navToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_borrowed);
+        setContentView(R.layout.activity_homepage);
 
         navToolbar = findViewById(R.id.navToolbar);
         setSupportActionBar(navToolbar);
