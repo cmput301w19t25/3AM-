86c453fd6e4391e98289acef4d740e981f35c960
ismaeelBM <ismaeel@ualberta.ca>
2019-03-06 20:44:16 -0700

    Added edit button UI only in profile page

--- app/src/main/java/comnickdchee/github/a3am/ProfileFragment.java
+++ app/src/main/java/comnickdchee/github/a3am/ProfileFragment.java
@@ -3,6 +3,7 @@ package comnickdchee.github.a3am;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,9 +11,15 @@ import android.view.ViewGroup;
 
 public class ProfileFragment extends Fragment {
 
+    private FloatingActionButton edit;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
+
+
+
 }
