6879f01ee2717fb55e6ccccec96d28491ab543dd
nickdchee <nickdchee@gmail.com>
2019-03-09 02:33:59 -0700

    trying to rig fab

--- app/src/main/java/comnickdchee/github/a3am/MyBooksFragment.java
+++ app/src/main/java/comnickdchee/github/a3am/MyBooksFragment.java
@@ -1,8 +1,10 @@
 package comnickdchee.github.a3am;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -26,6 +28,24 @@ public class MyBooksFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_borrowed, container, false);
 
         Bundle args = getArguments();
+
+        FloatingActionButton fab = view.findViewById(R.id.fabAddBookButton);
+
+        /**
+         * Passes intent to the NewBook activity, using StartActivityForResult
+         * to get back the results of an activity.
+         */
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), NewBookActivity.class);
+
+                // using this to get back the results of an intent
+                // Source: https://stackoverflow.com/questions/10407159/how-to-manage-startactivityforresult-on-android
+                startActivityForResult(intent, 1);
+            }
+        });
+
         ArrayList<String> data = new ArrayList<>();
         data.add("Hawwy Potta and the Prisoner Of Afghanistan");
         data.add("Hawwy Potta and the Sorcerer's Stoned");
