ed89d30573d557f48a63e183444d1939118c8ed3
nickdchee <nickdchee@gmail.com>
2019-03-09 15:08:32 -0700

    working on add book activity to pass intents

--- app/src/main/java/comnickdchee/github/a3am/NewBookActivity.java
+++ app/src/main/java/comnickdchee/github/a3am/NewBookActivity.java
@@ -1,11 +1,26 @@
 package comnickdchee.github.a3am;
 
+import android.media.Image;
+import android.support.design.widget.TextInputEditText;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
 
 public class NewBookActivity extends AppCompatActivity {
+
+    // text fields that user entered
+    private TextInputEditText bookTitleText;
+    private TextInputEditText bookAuthorText;
+    private TextInputEditText bookISBNText;
+
+    // close or finish activity buttons
+    private ImageView closeButton;
+    private ImageView addButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +28,26 @@ public class NewBookActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_book);
         Window window = this.getWindow();
         window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+
+        bookTitleText = findViewById(R.id.tietBookTitle);
+        bookAuthorText = findViewById(R.id.tietAuthor);
+        bookISBNText = findViewById(R.id.tietISBN);
+
+        closeButton = findViewById(R.id.ivCloseButton);
+        addButton = findViewById(R.id.ivFinishAddButton);
+
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
 
     }
 }
