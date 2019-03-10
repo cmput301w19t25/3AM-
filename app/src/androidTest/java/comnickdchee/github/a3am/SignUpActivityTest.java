adcb3d5a5c603b467c1b35c57f762661f262e22e
ttch501 <tatendachivasa@gmail.com>
2019-03-10 14:38:24 -0600

    still need to fix signup

--- app/src/androidTest/java/comnickdchee/github/a3am/SignUpActivityTest.java
+++ app/src/androidTest/java/comnickdchee/github/a3am/SignUpActivityTest.java
@@ -1,5 +1,7 @@
 package comnickdchee.github.a3am;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.support.test.rule.ActivityTestRule;
 import android.view.View;
 
@@ -7,19 +9,23 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import android.support.test.runner.AndroidJUnit4;
-
 import static org.junit.Assert.*;
 
 public class SignUpActivityTest {
     @Rule
     public ActivityTestRule<SignUpActivity> nActivityTestRule = new ActivityTestRule<SignUpActivity>(SignUpActivity.class);
+    //public ActivityTestRule<SignUpActivity> mActivityTestRule = new ActivityTestRule<SignUpActivity>(SignUpActivity.class);
+
 
     private SignUpActivity nActivity = null;
+    //private SignUpActivity mActivity = null;
+
+    //Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(BorrowedActivity.class.getName(),null,false);//monitor to show when second activity opens
 
     @Before //before you execute the test
     public void setUp() throws Exception {
         nActivity = nActivityTestRule.getActivity();
+
 
     }
 
@@ -27,9 +33,29 @@ public class SignUpActivityTest {
     @Test
     public void testlaunch() {
 
-        View view = nActivity.findViewById(R.id.PasswordReg);
-        assertNotNull(view);
+        View passwordField = nActivity.findViewById(R.id.PasswordReg);
+        assertNotNull(passwordField);
+
     }
+
+    //@Test
+   // public void TestLaunchofsigninwhenbuttonisclicked(){
+        //mActivity = nActivityTestRule.getActivity();
+        //assertNotNull(mActivity.findViewById(R.id.RegisterConfirm));
+        //onView(withId(R.id.userName)).perform(typeText("myself"));
+        //onView(withId(R.id.EmailReg).perform(typeText("myself@ualberta.ca")));
+        //onView(withId(R.id.PasswordReg)).perform(typeText("myself1234"));
+        //onView(withId(R.id.address)).perform(typeText("421 Edinburgh"));
+        //onView(withId(R.id.phoneNumber)).perform(typeText("0777890465")),closeSoftKeyboard();
+
+
+
+
+        //onView(withId(R.id.RegisterConfirm)).perform(click());
+        //Activity signinActivity = getInstrumentation().waitForMonitorWithTimeout(monitor,5000); //timeout is in milli-seconds
+        //assertNotNull(signinActivity);
+        //BorrowedActivity.finish();
+    //}
 
 
     @After // after executing the test case
