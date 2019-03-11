06e6d6138e230f60b1fcebb2c6610014859eb838
ttch501 <tatendachivasa@gmail.com>
2019-03-10 18:09:00 -0600

    fixed test

--- app/src/androidTest/java/comnickdchee/github/a3am/SignUpActivityTest.java
+++ app/src/androidTest/java/comnickdchee/github/a3am/SignUpActivityTest.java
@@ -13,7 +13,6 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-import comnickdchee.github.a3am.Models.User;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
@@ -23,13 +22,14 @@ import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
+import org.junit.runner.RunWith;
+
 public class SignUpActivityTest {
     @Rule
     public ActivityTestRule<SignUpActivity> nActivityTestRule = new ActivityTestRule<SignUpActivity>(SignUpActivity.class);
-    //public ActivityTestRule<SignUpActivity> mActivityTestRule = new ActivityTestRule<SignUpActivity>(SignUpActivity.class);
+    //public ActivityTestRule<BorrowedActivity> bActivityTestRule = new ActivityTestRule<BorrowedActivity>(SignUpActivity.class);
 
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
-    ;
     private SignUpActivity nActivity = null;
     //private SignUpActivity mActivity = null;
 
@@ -52,33 +52,27 @@ public class SignUpActivityTest {
 
     }
 
+
     @Test
     public void TestLaunchofsigninwhenbuttonisclicked(){
         SystemClock.sleep(800);
         assertNotNull(nActivity.findViewById(R.id.RegisterConfirm));
-        onView(withId(R.id.userName)).perform(typeText("two"));
-        onView(withId(R.id.EmailReg)).perform(typeText("two@gmail.com"));
-        onView(withId(R.id.PasswordReg)).perform(typeText("one1234"));
-        onView(withId(R.id.address)).perform(typeText("421 NY"));
+        onView(withId(R.id.userName)).perform(typeText("one113")).perform(closeSoftKeyboard());
+        onView(withId(R.id.EmailReg)).perform(typeText("one113@gmail.com")).perform(closeSoftKeyboard());
+        onView(withId(R.id.PasswordReg)).perform(typeText("one1234")).perform(closeSoftKeyboard());
+        onView(withId(R.id.address)).perform(typeText("421 NY")).perform(closeSoftKeyboard());
         onView(withId(R.id.phoneNumber)).perform(typeText("7778904605")).perform(closeSoftKeyboard());
         //Thread.sleep(250)
 
         onView(withId(R.id.RegisterConfirm)).perform(click());
-
-       // Activity signinActivity = getInstrumentation().waitForMonitorWithTimeout(monitor,5000); //timeout is in milli-seconds
-        Activity BorrowedActivity = getInstrumentation().waitForMonitor(monitor);
-        assertNotNull(BorrowedActivity);
-        //BorrowedActivity.finish();
     }
 
 
     @After // after executing the test case
     public void tearDown() throws Exception {
 
-        //User
         mAuth.getCurrentUser().delete();
-        nActivity= null;
-       // mActivity = null;
+
 
     }
 }
