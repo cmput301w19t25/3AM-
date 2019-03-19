49e191a50e2b0d95a650bced58fc880dc870fe89
nickdchee <nickdchee@gmail.com>
2019-03-19 14:02:43 -0600

    pulled search and changed searchactivity toolbar

--- app/src/main/java/comnickdchee/github/a3am/Activities/SearchResultsActivity.java
+++ app/src/main/java/comnickdchee/github/a3am/Activities/SearchResultsActivity.java
@@ -1,14 +1,20 @@
 package comnickdchee.github.a3am.Activities;
 
 import android.app.SearchManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.provider.SearchRecentSuggestions;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.Window;
+import android.widget.TextView;
 
 import comnickdchee.github.a3am.MySuggestionProvider;
 import comnickdchee.github.a3am.R;
@@ -16,6 +22,7 @@ import comnickdchee.github.a3am.R;
 public class SearchResultsActivity extends AppCompatActivity {
 
     private Toolbar navToolbar;
+    private TextView tvSearchTitle;
     private static final String TAG = "SearchResultsActivity";
 
     @Override
@@ -24,10 +31,19 @@ public class SearchResultsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
         navToolbar = findViewById(R.id.navToolbar);
+
+        // TODO: Change this so that it reads title, or better, set the title in each activity.
+        tvSearchTitle = findViewById(R.id.tvUsername);
+        tvSearchTitle.setText("Search Results");
+
         setSupportActionBar(navToolbar);
+
+        // Create a back button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
         Window window = this.getWindow();
         window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
-
 
         handleIntent(getIntent());
     }
@@ -40,12 +56,39 @@ public class SearchResultsActivity extends AppCompatActivity {
     private void handleIntent(Intent intent) {
 
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            // Save the most recent query for later search suggestion
             String query = intent.getStringExtra(SearchManager.QUERY);
             SearchRecentSuggestions suggestions = new SearchRecentSuggestions(this,
                     MySuggestionProvider.AUTHORITY, MySuggestionProvider.MODE);
             suggestions.saveRecentQuery(query, null);
+
+            // Perform search with the given query
+
         }
 
+    }
+
+    /**
+     * Inflate the menu when the SearchView item is pressed with the
+     * search menu. The toolbar gets a search view button implemented
+     * on the right side.
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+
+        ComponentName componentName = new ComponentName(this, SearchResultsActivity.class);
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(componentName));
+
+        return true;
     }
 
 }
