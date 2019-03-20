cad2b8648eaec3e364f5f0fa42d0742ba55b07b4
ismaeelBM <ismaeel@ualberta.ca>
2019-03-19 23:11:52 -0600

    Saved state for Getting JSon from URL

--- app/src/main/java/comnickdchee/github/a3am/Activities/NewBookActivity.java
+++ app/src/main/java/comnickdchee/github/a3am/Activities/NewBookActivity.java
@@ -3,6 +3,7 @@ package comnickdchee.github.a3am.Activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.media.Image;
+import android.os.AsyncTask;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TextInputEditText;
 import android.support.v4.content.ContextCompat;
@@ -14,13 +15,23 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.bumptech.glide.load.data.HttpUrlFetcher;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 
 import comnickdchee.github.a3am.Models.Book;
+import comnickdchee.github.a3am.Barcode.GoogleAPIBooks;
 import comnickdchee.github.a3am.R;
 
 import android.app.Activity;
@@ -36,6 +47,9 @@ import android.widget.TextView;
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class NewBookActivity extends AppCompatActivity {
 
@@ -87,11 +101,17 @@ public class NewBookActivity extends AppCompatActivity {
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(this, BarcodeCaptureActivity.class);
-                intent.putExtra(BarcodeCaptureActivity.AutoFocus, autoFocus.isChecked());
-                intent.putExtra(BarcodeCaptureActivity.UseFlash, useFlash.isChecked());
+                //ISBN : 9780773547971 --> Should give book title : "Promise and Challenge of Party Primary Elections"
+                String isbn = "9780773547971";
+                String urlISBN = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn;
+                try {
+                    URL url = new URL(urlISBN);
+                    JSONObject = Jsonfrom
 
-                startActivityForResult(intent, RC_BARCODE_CAPTURE);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+
             }
         });
 
@@ -111,6 +131,62 @@ public class NewBookActivity extends AppCompatActivity {
         DatabaseReference bRef = fD.getReference("BooksList").child(str);
         bRef.setValue(newBook);
         dRef.setValue(ISBN);
+    }
+
+    public class GetDataSync extends AsyncTask<Void, Void, Void> {
+
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                getData();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            txtUser.setText(saldo);
+        }
+    }
+
+    private void getData() throws IOException, JSONException {
+        JSONObject json = readJsonFromUrl("http://piggybank.wordmediavormgever.nl/getSaldo.php");
+        try {
+            String response = json.getString("saldo");
+            Log.e("AAAAAAAAA %s", response);
+            saldo = response;
+
+        } catch (JSONException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    private String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    public JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        InputStream is = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            JSONObject json = new JSONObject(jsonText);
+            return json;
+        } finally {
+            is.close();
+        }
     }
 
 }
