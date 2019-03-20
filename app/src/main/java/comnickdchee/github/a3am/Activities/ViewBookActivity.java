f704b430d3256590083719114e96440940dd58ea
soomyy <aaali@ualberta.ca>
2019-03-19 20:07:20 -0600

    working specify location

--- app/src/main/java/comnickdchee/github/a3am/Activities/ViewBookActivity.java
+++ app/src/main/java/comnickdchee/github/a3am/Activities/ViewBookActivity.java
@@ -1,11 +1,17 @@
 package comnickdchee.github.a3am.Activities;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -23,11 +29,24 @@ public class ViewBookActivity extends AppCompatActivity {
     private static ArrayList<String> requesters;
     private RequestersAdapter requestersAdapter;
     private RecyclerView.LayoutManager layoutManager;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_book);
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(ViewBookActivity.this, MapsActivity.class);
+                ViewBookActivity.this.startActivity(myIntent);
+                }
+        });
+
+
         Window window = this.getWindow();
         window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
 
