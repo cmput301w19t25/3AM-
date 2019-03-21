53e8596001ed6e976822d59fcaad8f1deb8120ce
soomyy <aaali@ualberta.ca>
2019-03-20 19:31:46 -0600

    navigation arrow & some profile things

--- app/src/main/java/comnickdchee/github/a3am/Activities/BorrowedProfileActivity.java
+++ app/src/main/java/comnickdchee/github/a3am/Activities/BorrowedProfileActivity.java
@@ -3,6 +3,7 @@ package comnickdchee.github.a3am.Activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,11 +15,21 @@ public class BorrowedProfileActivity extends AppCompatActivity {
 
     Button button1;
     Button button2;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_borrowed_profile);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        if (toolbar != null)
+        {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            //getSupportActionBar().setElevation(0); // or other
+        }
 
         button1 = (Button) findViewById(R.id.button3);
         button1.setOnClickListener(new View.OnClickListener() {
