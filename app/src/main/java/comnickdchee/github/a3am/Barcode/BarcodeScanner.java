1689bc424ed5353b1c731315c01e91c06f11117f
ismaeelBM <ismaeel@ualberta.ca>
2019-03-23 14:36:31 -0600

    Added real time permission request

--- app/src/main/java/comnickdchee/github/a3am/Barcode/BarcodeScanner.java
+++ app/src/main/java/comnickdchee/github/a3am/Barcode/BarcodeScanner.java
@@ -1,7 +1,15 @@
 package comnickdchee.github.a3am.Barcode;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,6 +23,7 @@ import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class BarcodeScanner extends Activity implements ZXingScannerView.ResultHandler {
 
+    private int CAMERA_PERMISSION_CODE = 1;
     private ZXingScannerView mScannerView;
     private static final String TAG = "BarcodeScanner";
 
@@ -23,6 +32,60 @@ public class BarcodeScanner extends Activity implements ZXingScannerView.ResultH
         super.onCreate(state);
         mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view
         setContentView(mScannerView);                // Set the scanner view as the content view
+        if (ContextCompat.checkSelfPermission(BarcodeScanner.this,
+                Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+
+        }
+        else
+        {
+            requestCameraPermission();
+        }
+
+
+    }
+
+    private void requestCameraPermission (){
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.CAMERA))
+        {
+            // This is needed if we need to give an extra explanation
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission needed")
+                    .setMessage("Need camera permission for Barcode Scanner")
+                    .setPositiveButton("Allow", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            ActivityCompat.requestPermissions(BarcodeScanner.this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+                            dialogInterface.dismiss();
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                            finish();
+                        }
+                    })
+                    .create().show();
+
+        }
+        else
+        {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+            } else {
+                Toast.makeText(this,"Permission Denied for Camera",Toast.LENGTH_SHORT).show();
+                finish();
+            }
+
+        }
     }
 
     @Override
@@ -30,12 +93,21 @@ public class BarcodeScanner extends Activity implements ZXingScannerView.ResultH
         super.onResume();
         mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
         mScannerView.startCamera();          // Start camera on resume
+        
+
     }
 
     @Override
     public void onPause() {
         super.onPause();
         mScannerView.stopCamera();           // Stop camera on pause
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        mScannerView.stopCamera();
+        finish();
     }
 
     @Override
