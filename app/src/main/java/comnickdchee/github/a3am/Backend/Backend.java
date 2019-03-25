d67cdb2bddbc6c1ad2054de72a5a1bcf7118e5a7
zaheenrahman <zaheen@ualberta.ca>
2019-03-25 10:52:49 -0600

    ImageAdding Functions added

--- app/src/main/java/comnickdchee/github/a3am/Backend/Backend.java
+++ app/src/main/java/comnickdchee/github/a3am/Backend/Backend.java
@@ -1,9 +1,12 @@
 package comnickdchee.github.a3am.Backend;
 
+import android.net.Uri;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.util.Log;
+import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -11,6 +14,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -21,6 +27,7 @@ import comnickdchee.github.a3am.Models.ExchangeType;
 import comnickdchee.github.a3am.Models.IOwner;
 import comnickdchee.github.a3am.Models.Status;
 import comnickdchee.github.a3am.Models.User;
+import comnickdchee.github.a3am.R;
 
 /**
  * Backend class that handles all the logic
@@ -499,5 +506,4 @@ public class Backend {
     public void setCurrentRequestedBooks(ArrayList<Book> requestedBooks) {
         mCurrentRequestedBooks = requestedBooks;
     }
-
 }
