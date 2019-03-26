595ef65a45279563c4c47065d4a508c02f3b226a
ismaeelBM <ismaeel@ualberta.ca>
2019-03-26 17:14:44 -0600

    Edit Activity created

---
+++ app/src/main/java/comnickdchee/github/a3am/Activities/EditProfile.java
@@ -0,0 +1,18 @@
+package comnickdchee.github.a3am.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import comnickdchee.github.a3am.R;
+
+public class EditProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+
+    }
+
+
+}
