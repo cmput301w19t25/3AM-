095c65ef2818cdabaab2688167d4bf0c2a0efa8e
ismaeelBM <ismaeel@ualberta.ca>
2019-03-29 20:09:04 -0600

    Added no data view for Borrowed tab

--- app/src/main/java/comnickdchee/github/a3am/Fragments/ActionsFragment.java
+++ app/src/main/java/comnickdchee/github/a3am/Fragments/ActionsFragment.java
@@ -60,21 +60,6 @@ public class ActionsFragment extends Fragment {
         recyclerView.setVisibility(View.VISIBLE);
         noDataView.setVisibility(View.INVISIBLE);
 
-
-        // Use the backend singleton to load the requested books,
-        // and then we add the requested books to the list to show on
-        // the fragment's recycler view
-
-        /**
-         * TODO: I will mimic adding a book using the addBook method of the backend singleton,
-         * which will be done first by pushing the user class to the "users" table. Then, we
-         * navigate to this actions fragment and perform add book.
-         */
-
-        // Add the book using the user data and update the tables
-//        Book testBook = new Book("12345", "Harry Potter", "JK Rowling", backend.getFirebaseUser().getUid());
-//        backend.addBook(testBook);
-
         //Create and return the recyclerView
         ActionsTabAdapter adapter = new ActionsTabAdapter(getActivity(), actionsBooksList);
         recyclerView.setAdapter(adapter);
